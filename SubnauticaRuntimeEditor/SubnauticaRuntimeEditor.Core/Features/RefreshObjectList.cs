b16931b1c079d13e9f7a94bcf5fe62e868330522
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-12-23 22:48:25 -0500

    Auto refresh option

---
+++ SubnauticaRuntimeEditor/SubnauticaRuntimeEditor.Core/Features/RefreshObjectList.cs
@@ -0,0 +1,21 @@
+﻿using SubnauticaRuntimeEditor.Core.Utils.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubnauticaRuntimeEditor.Core
+{
+    public class RefreshObjectList : FeatureBase<RefreshObjectList>
+    {
+        public static RefreshObjectList main;
+
+        protected override void Initialize(InitSettings initSettings)
+        {
+            DisplayName = "Auto refresh (slow!)";
+            Enabled = false;
+            main = this;
+        }
+    }
+}
