7408cfd87be2e91e1ba53a253a4a78427c11b5c3
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-03-30 19:34:57 -0400

    Update MainCameraControlPatches.cs

--- CreatureMorphs/CreatureMorphs/Patches/MainCameraControlPatches.cs
+++ CreatureMorphs/CreatureMorphs/Patches/MainCameraControlPatches.cs
@@ -13,10 +13,12 @@ internal static class MainCameraControlPatches
     {
         var morphedCreature = Morphing.main.GetCurrentMorph();
         if (morphedCreature == null || !morphedCreature.BeingControlled) return;
-
-        var lookDelta = GameInput.GetLookDelta();
-        angleX = Mathf.Clamp(angleX - lookDelta.y, -90, 90);
-        angleY += lookDelta.x;
+        if (Time.timeScale > 0.01f)
+        {
+            var lookDelta = GameInput.GetLookDelta();
+            angleX = Mathf.Clamp(angleX - lookDelta.y, -90, 90);
+            angleY += lookDelta.x;
+        }
 
         __instance.transform.eulerAngles = new Vector3(angleX, angleY, 0f);
         __instance.transform.position = morphedCreature.transform.position + __instance.transform.forward * -morphedCreature.morph.cameraFollowDistance;
