b4756e8161caba6a8ea3e54e688dd1674ec13bbd
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-04-08 19:44:29 -0400

    up version

--- CustomInventoryColors/CustomInventoryColors/PluginInfo.cs
+++ CustomInventoryColors/CustomInventoryColors/PluginInfo.cs
@@ -2,5 +2,5 @@
 {
     public const string GUID = "com.lee23.custominventorycolors";
     public const string Name = "Custom Inventory Colors";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
 }
