5b6b3f52e13ac913547951050133b92765e41893
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-06-02 01:20:37 -0400

    Update SeaVoyagerPrefab.cs

--- SeaVoyager/SeaVoyager/Prefabs/Vehicles/SeaVoyagerPrefab.cs
+++ SeaVoyager/SeaVoyager/Prefabs/Vehicles/SeaVoyagerPrefab.cs
@@ -121,7 +121,7 @@ public class SeaVoyagerPrefab
         vfxConstructing.Regenerate();
         // Don't want it tipping over...
         var stabilizier = prefab.AddComponent<Stabilizer>();
-        stabilizier.uprightAccelerationStiffness = 600f;
+        stabilizier.uprightAccelerationStiffness = 10f;
         // Some components might need this. I don't WANT it to take damage though, so I will just give it a LOT of health.
         var liveMixin = prefab.AddComponent<LiveMixin>();
         var lmData = ScriptableObject.CreateInstance<LiveMixinData>();
