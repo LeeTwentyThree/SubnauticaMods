777fe458ad75555834f2b5dc32c73396ed9855d6
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-05 12:03:16 -0400

    Add LongerSigns mod

---
+++ LongerSigns/LongerSigns/Patches.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using UnityEngine.UI;
+
+namespace LongerSigns;
+
+public static class Patches
+{
+    [HarmonyPatch(typeof(uGUI_SignInput), nameof(uGUI_SignInput.Awake))]
+    [HarmonyPostfix]
+    public static void uGUI_SignInputAwakePostfix(uGUI_SignInput __instance)
+    {
+        var inputField = __instance.GetComponentInChildren<uGUI_InputField>();
+        if (inputField == null)
+            return;
+
+        inputField.characterLimit = int.MaxValue;
+
+        // inputField.lineLimit = int.MaxValue;
+        // inputField.lineType = TMPro.TMP_InputField.LineType.MultiLineNewline;
+    }
+}
