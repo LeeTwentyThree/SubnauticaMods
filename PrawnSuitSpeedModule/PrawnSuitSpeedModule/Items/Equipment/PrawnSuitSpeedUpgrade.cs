435cb500155d1d066e98a61aa895616aa4df3da1
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-05 12:53:38 -0400

    Create PrawnSuitSpeedModule project

---
+++ PrawnSuitSpeedModule/PrawnSuitSpeedModule/Items/Equipment/PrawnSuitSpeedUpgrade.cs
@@ -0,0 +1,26 @@
+﻿using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Assets.PrefabTemplates;
+using Nautilus.Crafting;
+using Nautilus.Utility;
+using System.IO;
+using Ingredient = CraftData.Ingredient;
+
+namespace PrawnSuitSpeedModule.Items.Equipment;
+public static class PrawnSuitSpeedUpgrade
+{
+    public static PrefabInfo Info { get; } = PrefabInfo
+        .WithTechType("PrawnSuitSpeedModule", "Prawn Suit Speed Module", "Powerful module that makes me go yes.")
+        .WithIcon(ImageUtils.LoadSpriteFromFile(Path.Combine(Path.GetDirectoryName(Plugin.Assembly.Location), "Assets", "PrawnSuitSpeedModuleIcon.png")));
+
+    public static void Register()
+    {
+        var customPrefab = new CustomPrefab(Info);
+
+        var moduleModel = new CloneTemplate(Info, TechType.VehicleHullModule1);
+        customPrefab.SetGameObject(moduleModel);
+        customPrefab.SetRecipe(new RecipeData(new Ingredient(TechType.Copper, 2)));
+        customPrefab.SetVehicleUpgradeModule(EquipmentType.ExosuitModule);
+        customPrefab.Register();
+    }
+}
