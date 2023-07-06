d3cb8b563f4ca242fd1c7d80410d8550bf27f22e
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-05 20:46:19 -0400

    Work on exo speed upgrade

--- PrawnSuitSpeedModule/PrawnSuitSpeedModule/Items/Equipment/PrawnSuitSpeedUpgrade.cs
+++ PrawnSuitSpeedModule/PrawnSuitSpeedModule/Items/Equipment/PrawnSuitSpeedUpgrade.cs
@@ -3,6 +3,7 @@ using Nautilus.Assets.Gadgets;
 using Nautilus.Assets.PrefabTemplates;
 using Nautilus.Crafting;
 using Nautilus.Utility;
+using PrawnSuitSpeedModule.Mono;
 using System.IO;
 using Ingredient = CraftData.Ingredient;
 
@@ -10,7 +11,7 @@ namespace PrawnSuitSpeedModule.Items.Equipment;
 public static class PrawnSuitSpeedUpgrade
 {
     public static PrefabInfo Info { get; } = PrefabInfo
-        .WithTechType("PrawnSuitSpeedModule", "Prawn Suit Speed Module", "Powerful module that makes me go yes.")
+        .WithTechType("PrawnSuitSpeedModule", "Prawn Suit Speed Module", "Allows the Prawn Suit walking mechanism to enter overdrive. Multiple modules may be installed simultaneously.")
         .WithIcon(ImageUtils.LoadSpriteFromFile(Path.Combine(Path.GetDirectoryName(Plugin.Assembly.Location), "Assets", "PrawnSuitSpeedModuleIcon.png")));
 
     public static void Register()
@@ -19,8 +20,17 @@ public static class PrawnSuitSpeedUpgrade
 
         var moduleModel = new CloneTemplate(Info, TechType.VehicleHullModule1);
         customPrefab.SetGameObject(moduleModel);
-        customPrefab.SetRecipe(new RecipeData(new Ingredient(TechType.Copper, 2)));
-        customPrefab.SetVehicleUpgradeModule(EquipmentType.ExosuitModule);
+        customPrefab.SetRecipe(new RecipeData(new Ingredient(TechType.Nickel, 2), new Ingredient(TechType.Polyaniline, 1)))
+            .WithFabricatorType(CraftTree.Type.SeamothUpgrades)
+            .WithStepsToFabricatorTab("ExosuitModules");
+        customPrefab.SetVehicleUpgradeModule(EquipmentType.ExosuitModule)
+            .WithOnModuleAdded(OnUpdateModule) 
+            .WithOnModuleRemoved(OnUpdateModule); 
         customPrefab.Register();
+    }
+
+    private static void OnUpdateModule(Vehicle vehicle, int num)
+    {
+        vehicle.gameObject.EnsureComponent<PrawnSuitSpeedHandler>().UpdateSpeed();
     }
 }
---
+++ PrawnSuitSpeedModule/PrawnSuitSpeedModule/Mono/PrawnSuitSpeedHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace PrawnSuitSpeedModule.Mono;
+
+internal class PrawnSuitSpeedHandler : MonoBehaviour
+{
+    private Exosuit exosuit;
+
+    private float defaultForwardForce = 8.2f;
+    private float defaultBackwardForce = 3f;
+    private float defaultSidewardForce = 4.2f;
+
+    private void Awake()
+    {
+        exosuit = GetComponent<Exosuit>();
+    }
+
+    public void UpdateSpeed()
+    {
+        if (exosuit == null)
+            return;
+
+        float multiplier = GetForceMultiplier(exosuit);
+
+        exosuit.forwardForce = defaultForwardForce * multiplier;
+        exosuit.backwardForce = defaultBackwardForce * multiplier;
+        exosuit.sidewardForce = defaultSidewardForce * multiplier;
+    }
+    
+    private static float GetForceMultiplier(Exosuit exosuit)
+    {
+        return Mathf.Clamp(exosuit.modules.GetCount(Items.Equipment.PrawnSuitSpeedUpgrade.Info.TechType) + 1, 1, 10);
+    }
+}
