640aeee12132640de970b7fbb0b84a5dfb9e6368
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-17 14:49:33 -0400

    LoseEverything mod

---
+++ LoseEverything/LoseEverything/Config.cs
@@ -0,0 +1,13 @@
+﻿using Nautilus.Json;
+using Nautilus.Options.Attributes;
+
+namespace LoseEverything;
+
+[Menu("Lose Everything")]
+internal class Config : ConfigFile
+{
+    [Toggle("Lose all items on death")]
+    public bool LoseItemsOnDeath = true;
+    [Toggle("Lose all equipment on death")]
+    public bool LoseEquipmentOnDeath;
+}
---
+++ LoseEverything/LoseEverything/Patches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+
+namespace LoseEverything;
+
+[HarmonyPatch]
+internal static class Patches
+{
+    [HarmonyPrefix()]
+    [HarmonyPatch(typeof(Inventory), nameof(Inventory.LoseItems))]
+    public static bool InventoryLoseItemsPrefix(Inventory __instance, ref bool __result)
+    {
+        if (!Plugin.Config.LoseItemsOnDeath && !Plugin.Config.LoseEquipmentOnDeath)
+        {
+            return true;
+        }
+        var list = new List<InventoryItem>();
+        foreach (InventoryItem inventoryItem in __instance.container)
+        {
+            if (Plugin.Config.LoseItemsOnDeath || inventoryItem.item.destroyOnDeath)
+            {
+                list.Add(inventoryItem);
+            }
+        }
+        foreach (InventoryItem inventoryItem2 in (IItemsContainer)__instance.equipment)
+        {
+            if (Plugin.Config.LoseEquipmentOnDeath || inventoryItem2.item.destroyOnDeath)
+            {
+                list.Add(inventoryItem2);
+            }
+        }
+        __result = false;
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (__instance.InternalDropItem(list[i].item, false))
+            {
+                __result = true;
+            }
+        }
+        return false;
+    }
+}
