6fa8267ebf1a68ee3816a9b2bfd88fe269c628c2
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-07-17 14:50:51 -0400

    Update Config.cs

--- LoseEverything/LoseEverything/Config.cs
+++ LoseEverything/LoseEverything/Config.cs
@@ -9,5 +9,5 @@ internal class Config : ConfigFile
     [Toggle("Lose all items on death")]
     public bool LoseItemsOnDeath = true;
     [Toggle("Lose all equipment on death")]
-    public bool LoseEquipmentOnDeath;
+    public bool LoseEquipmentOnDeath = true;
 }
