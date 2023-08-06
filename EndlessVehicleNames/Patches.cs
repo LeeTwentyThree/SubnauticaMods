a88bd5f1ef6d9f4dc7fa0d0e8f4bfb85cba19f89
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-06 13:23:55 -0400

    EndlessVehicleNames source code

---
+++ EndlessVehicleNames/Patches.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+
+namespace EndlessVehicleNames;
+
+[HarmonyPatch]
+internal static class Patches
+{
+    [HarmonyPatch(typeof(SubNameInput), nameof(SubNameInput.Start))]
+    [HarmonyPostfix]
+    public static void SubNameInputStartPostfix(SubNameInput __instance)
+    {
+        if (__instance.inputField != null)
+        {
+            __instance.inputField.characterLimit = 0;
+        }
+    }
+
+    [HarmonyPatch(typeof(SubName), nameof(SubName.Awake))]
+    [HarmonyPostfix]
+    public static void SubNameAwakePostfix(SubName __instance)
+    {
+        if (__instance.hullName != null)
+        {
+            __instance.hullName.fontSizeMin = 1;
+        }
+    }
+}
