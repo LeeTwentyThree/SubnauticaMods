919e4b4f313e92841e68399149323efbfa61e0e4
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-08-17 20:40:06 -0400

    NoOutOfBoundsTeleport mod

---
+++ NoOutOfBoundsTeleport/Patches.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace NoOutOfBoundsTeleport;
+
+[HarmonyPatch]
+internal static class Patches
+{
+    [HarmonyPatch(typeof(OutOfBoundsWarp), nameof(OutOfBoundsWarp.Start))]
+    [HarmonyPostfix]
+    public static void OutOfBoundsWarpStartPostfix(OutOfBoundsWarp __instance)
+    {
+        UnityEngine.Object.Destroy(__instance);
+    }
+}
---
+++ NoOutOfBoundsTeleport/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace NoOutOfBoundsTeleport;
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    private void Awake()
+    {
+        // plugin startup logic
+        Logger = base.Logger;
+
+        // register harmony patches, if there are any
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
