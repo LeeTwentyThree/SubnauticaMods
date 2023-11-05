ca47613901ca9082f68162dabd96b67b26cd5035
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-11-04 20:00:57 -0400

    Podshell basics

---
+++ PodshellLeviathan/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Assets;
+using Nautilus.Handlers;
+using Nautilus.Utility;
+using PodshellLeviathan.Prefabs;
+using System.Reflection;
+using UnityEngine;
+
+namespace PodshellLeviathan;
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    internal static AssetBundle Assets { get; } = AssetBundleLoadingUtils.LoadFromAssetsFolder(Assembly, "podshellleviathan");
+
+    internal static PodshellLeviathanPrefab PodshellLeviathan { get; } = new PodshellLeviathanPrefab(
+        PrefabInfo.WithTechType("PodshellLeviathan", null, null));
+
+    private void Awake()
+    {
+        // set project-scoped logger instance
+        Logger = base.Logger;
+
+        LanguageHandler.RegisterLocalizationFolder();
+
+        // Initialize custom prefabs
+        InitializePrefabs();
+
+        // register harmony patches, if there are any
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void InitializePrefabs()
+    {
+        PodshellLeviathan.Register();
+    }
+}
---
+++ PodshellLeviathan/Prefabs/PodshellLeviathan.cs
@@ -0,0 +1,54 @@
+﻿using ECCLibrary;
+using ECCLibrary.Data;
+using Nautilus.Assets;
+using Nautilus.Utility;
+using System.Collections;
+using UnityEngine;
+
+namespace PodshellLeviathan.Prefabs;
+
+internal class PodshellLeviathanPrefab : CreatureAsset
+{
+    private const float kSwimSpeedPriority = 0.1f;
+    private const float kAvoidTerrainPriority = 0.8f;
+    private const float kStayAtLeashPriority = 0.1f;
+
+    private const float kStandardSwimVelocity = 4f;
+
+    public PodshellLeviathanPrefab(PrefabInfo prefabInfo) : base(prefabInfo)
+    {
+    }
+
+    protected override CreatureTemplate CreateTemplate()
+    {
+        var template = new CreatureTemplate(
+            Plugin.Assets.LoadAsset<GameObject>("PodshellLeviathanPrefab"),
+            BehaviourType.Leviathan,
+            EcoTargetType.Leviathan,
+            6000);
+
+        CreatureTemplateUtils.SetCreatureDataEssentials(template, LargeWorldEntity.CellLevel.VeryFar, 3000f, 0, new BehaviourLODData(150, 250, 300), 10000);
+        template.SwimRandomData = new SwimRandomData(kSwimSpeedPriority, kStandardSwimVelocity, new Vector3(30, 4, 30), 6f, 1f, true);
+        template.AvoidTerrainData = new AvoidTerrainData(kAvoidTerrainPriority, kStandardSwimVelocity, 30f, 30f);
+        template.StayAtLeashData = new StayAtLeashData(kStayAtLeashPriority, kStandardSwimVelocity, 140f);
+        template.CanBeInfected = false;
+        template.SizeDistribution = new AnimationCurve(new Keyframe(0f, 0.7f), new Keyframe(1f, 1f));
+        template.LocomotionData = new LocomotionData(10f, 0.1f, 3f, 0.5f);
+
+        return template;
+    }
+
+    protected override IEnumerator ModifyPrefab(GameObject prefab, CreatureComponents components)
+    {
+        var tailRoot = prefab.transform.Find("turtle_rigged/DO_NOT_TOUCH/root/cog/tail1").GetChild(0);
+        var tailTrailManager = new TrailManagerBuilder(components, tailRoot, 10, 3);
+        tailTrailManager.SetTrailArrayToAllChildren();
+        tailTrailManager.Apply();
+        yield break;
+    }
+
+    protected override void ApplyMaterials(GameObject prefab)
+    {
+        MaterialUtils.ApplySNShaders(prefab, 6, 1, 1, new PodshellMaterialModifier());
+    }
+}
