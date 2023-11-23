8a35862c160a4d645d91ef59350e621122ca9f6c
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-11-22 23:29:09 -0500

    Fix golden thunderstorm duration

--- WeatherMod/WeatherMod/WeatherEvents/GoldenThunderstorm.cs
+++ WeatherMod/WeatherMod/WeatherEvents/GoldenThunderstorm.cs
@@ -8,8 +8,8 @@ public class GoldenThunderstorm : WeatherEvent
     protected override GameObject EffectPrefab { get; } = Plugin.AssetBundle.LoadAsset<GameObject>("Weather_Freddy");
     protected override float DestroyDelay { get; } = 18;
     protected override FogSettings Fog { get; } = new FogSettings(0.002f, new Color(0.4f, 0.4f, 0.1f), 0.2f, 1f, 0.5f);
-    public override float MinDuration { get; } = 120;
-    public override float MaxDuration { get; } = 160;
+    public override float MinDuration { get; } = 105.87f;
+    public override float MaxDuration { get; } = 105.87f;
     public override ModSound AmbientSound { get; } = WeatherAudio.GoldenThunderstormLoop;
 
     protected override void OnEventBegin(GameObject effectPrefab)
