b51a8c553539876ecd400d31633c980247f32238
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-11-22 23:28:50 -0500

    Improve weather audio implementation

--- WeatherMod/WeatherMod/WeatherAudio.cs
+++ WeatherMod/WeatherMod/WeatherAudio.cs
@@ -4,6 +4,8 @@ namespace WeatherMod;
 
 public static class WeatherAudio
 {
+    private const string KSurfaceWeatherBuses = "bus:/master/SFX_for_pause/PDA_pause/all/SFX/creatures surface";
+    
     public static readonly ModSound[] ThunderSoundsNear = new[]
     {
         new ModSound("ThunderNear_1"),
@@ -18,19 +20,19 @@ public static class WeatherAudio
         new ModSound("ThunderFar_3")
     };
 
-    public static ModSound ThunderstormLoop { get; } = new ModSound("ThunderstormLoop2", "ThunderstormLoop");
+    public static ModSound ThunderstormLoop { get; } = new ModSound("ThunderstormLoop3", "ThunderstormLoop");
     public static ModSound WindyLoop { get; } = new ModSound("WindLoop_Heavy");
-    public static ModSound LightRainLoop { get; } = new ModSound("LightRainLoop");
+    public static ModSound LightRainLoop { get; } = new ModSound("LightRainLoop2", "LightRainLoop");
     public static ModSound GoldenThunderstormLoop { get; } = new ModSound("GoldenThunderstormLoop");
     
     public static void RegisterAll()
     {
-        ThunderSoundsNear.ForEach(s => s.Register(AudioUtils.BusPaths.SurfaceAmbient, 10, 400));
-        ThunderSoundsFar.ForEach(s => s.Register(AudioUtils.BusPaths.SurfaceAmbient, 40, 1000));
+        ThunderSoundsNear.ForEach(s => s.Register(AudioUtils.BusPaths.SurfaceAmbient, true, 10, 400));
+        ThunderSoundsFar.ForEach(s => s.Register(AudioUtils.BusPaths.SurfaceAmbient, true, 40, 1000));
         
-        ThunderstormLoop.Register(AudioUtils.BusPaths.SurfaceAmbient, -1f, -1f);
-        WindyLoop.Register(AudioUtils.BusPaths.SurfaceAmbient, -1f, -1f);
-        LightRainLoop.Register(AudioUtils.BusPaths.SurfaceAmbient, -1f, -1f);
-        GoldenThunderstormLoop.Register(AudioUtils.BusPaths.PlayerSFXs, -1f, -1f);
+        ThunderstormLoop.Register(KSurfaceWeatherBuses, false, -1f, -1f);
+        WindyLoop.Register(KSurfaceWeatherBuses, false, -1f, -1f);
+        LightRainLoop.Register(KSurfaceWeatherBuses, false, -1f, -1f);
+        GoldenThunderstormLoop.Register(AudioUtils.BusPaths.PlayerSFXs, false, -1f, -1f);
     }
 }
