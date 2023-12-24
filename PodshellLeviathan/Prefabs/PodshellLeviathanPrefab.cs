ec9fc9fe89d0421bcbe3bdcc43830f2f88dfd755
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-12-24 14:08:18 -0500

    Improve podshell navigation

--- PodshellLeviathan/Prefabs/PodshellLeviathanPrefab.cs
+++ PodshellLeviathan/Prefabs/PodshellLeviathanPrefab.cs
@@ -14,7 +14,7 @@ internal class PodshellLeviathanPrefab : CreatureAsset
     private const float kAvoidTerrainPriority = 0.8f;
     private const float kStayAtLeashPriority = 0.1f;
 
-    private const float kStandardSwimVelocity = 3f;
+    private const float kStandardSwimVelocity = 6f;
 
     public PodshellLeviathanPrefab(PrefabInfo prefabInfo) : base(prefabInfo)
     {
@@ -30,7 +30,7 @@ internal class PodshellLeviathanPrefab : CreatureAsset
 
         CreatureTemplateUtils.SetCreatureDataEssentials(template, LargeWorldEntity.CellLevel.VeryFar, 3000f, 0, new BehaviourLODData(150, 250, 300), 10000);
         template.SwimRandomData = new SwimRandomData(kSwimSpeedPriority, kStandardSwimVelocity, new Vector3(100, 4, 100), 5f, 1f, true);
-        template.AvoidTerrainData = new AvoidTerrainData(kAvoidTerrainPriority, kStandardSwimVelocity, 30f, 30f);
+        template.AvoidTerrainData = new AvoidTerrainData(kAvoidTerrainPriority, kStandardSwimVelocity, 20f, 20f);
         template.StayAtLeashData = new StayAtLeashData(kStayAtLeashPriority, kStandardSwimVelocity, 140f);
         template.CanBeInfected = false;
         template.SizeDistribution = new AnimationCurve(new Keyframe(0f, 0.7f), new Keyframe(1f, 1f));
