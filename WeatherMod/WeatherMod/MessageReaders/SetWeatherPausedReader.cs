1572b832b239f6f99a4ef2f3f29694aa6b339fda
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-01-10 20:51:45 -0500

    Improve weather API (epic weather 0.0.6)

---
+++ WeatherMod/WeatherMod/MessageReaders/SetWeatherPausedReader.cs
@@ -0,0 +1,15 @@
+﻿using Nautilus.Utility.ModMessages;
+
+namespace WeatherMod.MessageReaders;
+
+public class SetWeatherPausedReader : ModMessageReader
+{
+    protected override void OnReceiveMessage(ModMessage message)
+    {
+        if (message.Subject != "SetWeatherPaused") return;
+        
+        var paused = (bool)message.Contents[1];
+        
+        WeatherAPI.SetWeatherPaused(paused);
+    }
+}
---
+++ WeatherMod/WeatherMod/MessageReaders/SetWeatherReader.cs
@@ -0,0 +1,15 @@
+﻿using Nautilus.Utility.ModMessages;
+
+namespace WeatherMod.MessageReaders;
+
+public class SetWeatherReader : ModMessageReader
+{
+    protected override void OnReceiveMessage(ModMessage message)
+    {
+        if (message.Subject != "SetWeather") return;
+        
+        var weatherName = (string)message.Contents[0];
+        
+        WeatherAPI.SetWeatherEvent(weatherName);
+    }
+}
