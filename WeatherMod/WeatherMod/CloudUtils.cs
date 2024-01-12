ca95e0da12dabd790185fa2e10b9e1bbb4546919
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-01-12 16:51:57 -0500

    Fix annoying weather mod message

--- WeatherMod/WeatherMod/CloudUtils.cs
+++ WeatherMod/WeatherMod/CloudUtils.cs
@@ -53,7 +53,7 @@ public static class CloudUtils
     {
         if (_smokeVfxOriginal != null) return;
         
-        if (CrashedShipExploder.main == null) ErrorMessage.AddMessage("Weather mod error: AURORA NOT FOUND! Where did it go lol?");
+        if (CrashedShipExploder.main == null) Plugin.Logger.LogError("Weather mod error: AURORA NOT FOUND! Where did it go lol?");
         
         // "Failed cloud T_T X_X smh lol"
         _smokeVfxOriginal = CrashedShipExploder.main.transform
