206467065845d638f2a9ca239a39e1aba001642b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-01-17 20:58:55 -0500

    Add StandardActivityCurve for diurnal creatures

---
+++ DeExtinction/DeExtinctionUtils.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DeExtinction;
+
+public static class DeExtinctionUtils
+{
+    public static AnimationCurve StandardActivityCurve { get; } = new AnimationCurve(
+        new Keyframe(0, 0.3f, 0, 0, 0.333f, 0.333f),
+        new Keyframe(0.1f, 0.3f, 0, 0.035f, 0.333f, 0.535f),
+        new Keyframe(0.2f, 1f, 0, 0, 0.333f, 0.333f),
+        new Keyframe(0.8f, 1f, 0, 0, 0.333f, 0.333f),
+        new Keyframe(0.9f, 0.3f, 0.00999f, 0.00999f, 0.152f, 0.621f),
+        new Keyframe(1f, 0.3f, 0, 0, 0.333f, 0.333f)
+    );
+}
