a770c073241c44b067549445bfb11ecc391e20c6
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-04-20 00:46:14 -0400

    Shrink fish in mouth

--- DeExtinction/Mono/BirdGrabFish.cs
+++ DeExtinction/Mono/BirdGrabFish.cs
@@ -6,6 +6,7 @@ public class BirdGrabFish : MonoBehaviour
 {
     public Transform fishParent;
     private GameObject _heldFish;
+    private float _originalFishScale;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -35,6 +36,7 @@ public class BirdGrabFish : MonoBehaviour
     private void GrabFish(Creature fish)
     {
         _heldFish = fish.gameObject;
+        _originalFishScale = _heldFish.transform.localScale.x;
         fish.GetComponent<Rigidbody>().isKinematic = true;
         fish.gameObject.AddComponent<FishBleedOut>();
         var fishTransform = fish.transform;
@@ -47,20 +49,33 @@ public class BirdGrabFish : MonoBehaviour
         }
         Invoke(nameof(Kill), 2);
         Invoke(nameof(Eat), 6);
+        InvokeRepeating(nameof(Bite), 1f, 1f);
     }
 
+    private void Bite()
+    {
+        if (_heldFish) _heldFish.transform.localScale -= Vector3.one * (0.2f * _originalFishScale);
+    }
+    
     private void Kill()
     {
         if (_heldFish != null)
         {
             _heldFish.GetComponent<LiveMixin>().TakeDamage(60, transform.position);
+        }
+    }
+
+    private void Update()
+    {
+        if (_heldFish != null)
+        {
             _heldFish.transform.parent = fishParent;
             var rb = _heldFish.GetComponent<Rigidbody>();
             if (rb) rb.isKinematic = true;
             _heldFish.transform.localPosition = Vector3.zero;
         }
     }
-    
+
     private void Eat()
     {
         Destroy(_heldFish);
