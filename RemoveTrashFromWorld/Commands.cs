b732b0f38ac26df9ba32465a6243c361a8f86049
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-06-01 14:49:01 -0400

    Remove trash from world mod

---
+++ RemoveTrashFromWorld/Commands.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Nautilus.Commands;
+using UnityEngine;
+
+namespace RemoveTrashFromWorld;
+
+public static class Commands
+{
+    [ConsoleCommand("cleanup")]
+    public static void CleanUp(string itemId, float distanceFromPlayer)
+    {
+        var identifiers = UniqueIdentifier.identifiers;
+        foreach (var identifier in identifiers)
+        {
+            if (identifier.Value == null || identifier.Value is not PrefabIdentifier prefabIdentifier)
+            {
+                continue;
+            }
+
+            if (Vector3.Distance(prefabIdentifier.transform.position, MainCamera.camera.transform.position) >
+                distanceFromPlayer)
+            {
+                continue;
+            }
+            
+            if (ObjectMatchesId(prefabIdentifier, itemId))
+            {
+                UnityEngine.Object.Destroy(prefabIdentifier.gameObject);
+            }
+        }
+
+        var globalRoot = LargeWorldStreamer.main.globalRoot;
+        foreach (var prefabIdentifier in globalRoot.GetComponentsInChildren<PrefabIdentifier>())
+        {
+            if (Vector3.Distance(prefabIdentifier.transform.position, MainCamera.camera.transform.position) >
+                distanceFromPlayer)
+            {
+                continue;
+            }
+            
+            if (ObjectMatchesId(prefabIdentifier, itemId))
+            {
+                UnityEngine.Object.Destroy(prefabIdentifier.gameObject);
+            }
+        }
+    }
+
+    private static bool ObjectMatchesId(PrefabIdentifier identifier, string id)
+    {
+        if (identifier.ClassId.Equals(id, StringComparison.OrdinalIgnoreCase))
+            return true;
+        if (CraftData.GetTechType(identifier.gameObject).ToString().Equals(id, StringComparison.OrdinalIgnoreCase))
+            return true;
+        return false;
+    }
+}
---
+++ RemoveTrashFromWorld/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Handlers;
+
+namespace RemoveTrashFromWorld;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+    
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    private void Awake()
+    {
+        // set project-scoped logger instance
+        Logger = base.Logger;
+        
+        // Initialize custom prefabs
+        ConsoleCommandsHandler.RegisterConsoleCommands(typeof(Commands));
+
+        // register harmony patches, if there are any
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
