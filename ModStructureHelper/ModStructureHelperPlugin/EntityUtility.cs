cdc4ec5938d6b5f6cae5ddb5124140212b634983
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-06-11 21:50:27 -0400

    MAKE SURE UNIQUE IDS ARE PERSISTENT

--- ModStructureHelper/ModStructureHelperPlugin/EntityUtility.cs
+++ ModStructureHelper/ModStructureHelperPlugin/EntityUtility.cs
@@ -38,6 +38,7 @@ public static class EntityUtility
             obj.transform.position = data.position.ToVector3();
             obj.transform.rotation = data.rotation.ToQuaternion();
             obj.transform.localScale = data.scale.ToVector3();
+            obj.GetComponent<UniqueIdentifier>().Id = data.id;
         }
     }
 }
