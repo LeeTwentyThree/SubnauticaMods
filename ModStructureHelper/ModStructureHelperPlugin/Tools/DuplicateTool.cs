c7725351443a7f796bf5fbe4ac3a082b9cd8cb7b
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-06-15 17:20:26 -0400

    Fix duplication tool

--- ModStructureHelper/ModStructureHelperPlugin/Tools/DuplicateTool.cs
+++ ModStructureHelper/ModStructureHelperPlugin/Tools/DuplicateTool.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 namespace ModStructureHelperPlugin.Tools;
@@ -16,19 +18,39 @@ public class DuplicateTool : ToolBase
         DisableTool();
     }
 
-    private void DuplicateAllSelectedObjects()
+    private IEnumerator DuplicateAllSelectedObjects()
     {
         var duplicatedObjects = new List<GameObject>();
+        var loadedPrefabs = new Dictionary<string, GameObject>();
         foreach (var selected in SelectionManager.SelectedObjects)
         {
-            if (selected != null)
+            var prefabIdentifier = selected.GetComponent<PrefabIdentifier>();
+            
+            if (prefabIdentifier == null)
             {
-                duplicatedObjects.Add(StructureInstance.Main.SpawnPrefabIntoStructure(selected.gameObject));
+                ErrorMessage.AddMessage($"Cannot duplicate '{selected}'; this object has no prefab identifier!");
+                continue;
             }
+            
+            if (!loadedPrefabs.TryGetValue(prefabIdentifier.ClassId, out var prefab))
+            {
+                var task = UWE.PrefabDatabase.GetPrefabAsync(prefabIdentifier.ClassId);
+                yield return task;
+                if (!task.TryGetPrefab(out prefab)) ErrorMessage.AddMessage($"Failed to load prefab for prefab of Class ID {prefabIdentifier.ClassId}");
+                loadedPrefabs.Add(prefabIdentifier.ClassId, prefab);
+            }
+
+            var newCopy = Instantiate(prefab);
+            newCopy.transform.position = selected.transform.position;
+            newCopy.transform.rotation = selected.transform.rotation;
+            newCopy.transform.localScale = selected.transform.localScale;
+            
+            StructureInstance.Main.RegisterNewEntity(newCopy.GetComponent<PrefabIdentifier>());
+            duplicatedObjects.Add(newCopy);
         }
         SelectionManager.ClearSelection();
         duplicatedObjects.ForEach(SelectionManager.AddSelectedObject);
-        ErrorMessage.AddMessage($"Duplicated {duplicatedObjects.Count} objects.");
+        ErrorMessage.AddMessage($"Duplicated {duplicatedObjects.Count} object(s).");
     }
 
     protected override void OnToolDisabled() { }
