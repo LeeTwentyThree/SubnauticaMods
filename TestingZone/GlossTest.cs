63a0486fb8cfa4aec4fa0e5127e78b101e6e2dd6
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-07-20 18:22:09 -0400

    Test out gloss

---
+++ TestingZone/GlossTest.cs
@@ -0,0 +1,18 @@
+﻿using Nautilus.Assets;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace TestingZone;
+
+public static class GlossTest
+{
+    public static void Register()
+    {
+        var prefab = new CustomPrefab(PrefabInfo.WithTechType("GlossTest"));
+        var obj = Plugin.Bundle.LoadAsset<GameObject>("TestPlane");
+        PrefabUtils.AddBasicComponents(obj, prefab.Info.ClassID, prefab.Info.TechType, LargeWorldEntity.CellLevel.Medium);
+        MaterialUtils.ApplySNShaders(obj, 8f, 1f);
+        prefab.SetGameObject(obj);
+        prefab.Register();
+    }
+}
---
+++ TestingZone/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace TestingZone;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    public static AssetBundle Bundle { get; } = AssetBundleLoadingUtils.LoadFromAssetsFolder(Assembly, "testingzone");
+
+    private void Awake()
+    {
+        // set project-scoped logger instance
+        Logger = base.Logger;
+
+        // Initialize custom prefabs
+        InitializePrefabs();
+
+        // register harmony patches, if there are any
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void InitializePrefabs()
+    {
+        GlossTest.Register();
+    }
+}
