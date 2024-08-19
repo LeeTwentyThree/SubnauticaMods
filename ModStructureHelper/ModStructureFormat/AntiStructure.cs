fc50a0ddf5e3410dc9f35850a3a9d0c948ac46ed
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2024-08-18 23:36:14 -0400

    Add anti structure class

---
+++ ModStructureHelper/ModStructureFormat/AntiStructure.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+
+namespace ModStructureFormat;
+
+/// <summary>
+/// Represents a group of object IDs to be removed from the world.
+/// </summary>
+[Serializable]
+public class AntiStructure
+{
+    public AntiStructure(List<string> uniqueIds)
+    {
+        UniqueIds = uniqueIds;
+    }
+    
+    public List<string> UniqueIds { get; }
+    
+    public static Structure LoadFromFile(string jsonFilePath)
+    {
+        return JsonConvert.DeserializeObject<Structure>(File.ReadAllText(jsonFilePath));
+    }
+    
+    public void SaveToFile(string jsonFilePath)
+    {
+        File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(this));
+    }
+}
