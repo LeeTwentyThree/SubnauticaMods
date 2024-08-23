a70ff0ac0b41846b0599bcbee64294a462352eeb
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2024-08-23 17:54:03 -0400

    Implement new bomb crate model

--- TheRedPlague/TheRedPlague/PrefabFiles/AlterraBomb.cs
+++ TheRedPlague/TheRedPlague/PrefabFiles/AlterraBomb.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using Nautilus.Assets;
 using Nautilus.Assets.PrefabTemplates;
+using Nautilus.Utility;
 using TheRedPlague.Mono.Tools;
 using UnityEngine;
 
@@ -13,28 +14,29 @@ public static class AlterraBomb
     public static void Register()
     {
         var customPrefab = new CustomPrefab(Info);
-        customPrefab.SetGameObject(new CloneTemplate(Info, "60f7bcb6-1d0f-40a2-ab68-e1fae7370d4a")
-        {
-            ModifyPrefabAsync = ModifyPrefabAsync
-        });
+        customPrefab.SetGameObject(GetPrefabAsync);
         customPrefab.Register();
     }
 
-    private static IEnumerator ModifyPrefabAsync(GameObject prefab)
+    private static IEnumerator GetPrefabAsync(IOut<GameObject> prefab)
     {
+        var obj = Object.Instantiate(Plugin.AssetBundle.LoadAsset<GameObject>("ExplosiveCratePrefab"));
+        obj.SetActive(false);
+        PrefabUtils.AddBasicComponents(obj, Info.ClassID, Info.TechType, LargeWorldEntity.CellLevel.Near);
+        MaterialUtils.ApplySNShaders(obj);
         var seamothTask = CraftData.GetPrefabForTechTypeAsync(TechType.Seamoth);
         yield return seamothTask;
         var seamothPrefab = seamothTask.GetResult();
         var destructionEffect = seamothPrefab.GetComponent<SeaMoth>().destructionEffect;
-        prefab.AddComponent<BombExplodeOnImpact>().explosionPrefab = destructionEffect;
-        var rb = prefab.GetComponent<Rigidbody>();
+        obj.AddComponent<BombExplodeOnImpact>().explosionPrefab = destructionEffect;
+        var rb = obj.AddComponent<Rigidbody>();
         rb.mass = 2;
         rb.useGravity = false;
         rb.isKinematic = true;
-        var wf = prefab.AddComponent<WorldForces>();
+        var wf = obj.AddComponent<WorldForces>();
         wf.useRigidbody = rb;
-        var renderer = prefab.GetComponentInChildren<Renderer>();
-        var materials = renderer.materials;
-        materials[0].color = new Color(0.7f, 0f, 0f);
+        obj.AddComponent<ConstructionObstacle>();
+        
+        prefab.Set(obj);
     }
 }
