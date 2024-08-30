6e789edf86f4c0d3ce314afbb3186155ce06309c
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-08-30 16:39:21 -0400

    Create ScaleToSize.cs

---
+++ TheRedPlague/TheRedPlague/Mono/VFX/ScaleToSize.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace TheRedPlague.Mono.VFX;
+
+public class ScaleToSize : MonoBehaviour
+{
+    public float scaleSpeed;
+
+    private float _currentScale;
+    private float _targetSize;
+
+    private void Start()
+    {
+        _currentScale = transform.position.x;
+    }
+
+    private void Update()
+    {
+        _currentScale = Mathf.MoveTowards(_currentScale, _targetSize, scaleSpeed * Time.deltaTime);
+        transform.localScale = new Vector3(_currentScale, _currentScale, _currentScale);
+    }
+
+    public void SetNewSize(float newSize)
+    {
+        _targetSize = newSize;
+    }
+
+    public void SetSizeInstantly(float newSize)
+    {
+        _targetSize = newSize;
+        _currentScale = newSize;
+    }
+}
