bd9a7224b45487e8f2882ae067aa28e9d34d600e
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2024-09-06 21:15:25 -0400

    Create Lil' Gargy mod

---
+++ LilGargy/LilGargy/Items/Equipment/LilGargyPrefab.cs
@@ -0,0 +1,53 @@
+﻿using Ingredient = CraftData.Ingredient;
+using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Crafting;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace LilGargy.Items.Equipment;
+
+public static class LilGargyPrefab
+{
+    public static PrefabInfo Info { get; } = PrefabInfo
+        .WithTechType("LilGargy")
+        .WithIcon(Plugin.Bundle.LoadAsset<Sprite>("LilGargyIcon"));
+
+    public static void Register()
+    {
+        var customPrefab = new CustomPrefab(Info);
+        
+        customPrefab.SetGameObject(GetGameObject);
+        customPrefab.SetRecipe(new RecipeData(new Ingredient(TechType.Titanium, 2)))
+            .WithFabricatorType(CraftTree.Type.Fabricator)
+            .WithStepsToFabricatorTab("Machines")
+            .WithCraftingTime(10);
+        customPrefab.SetEquipment(EquipmentType.Hand);
+        customPrefab.Register();
+    }
+
+    private static GameObject GetGameObject()
+    {
+        var prefab = Object.Instantiate(Plugin.Bundle.LoadAsset<GameObject>("LilGargyPrefab"));
+        prefab.SetActive(false);
+        PrefabUtils.AddBasicComponents(prefab, Info.ClassID, Info.TechType, LargeWorldEntity.CellLevel.Near);
+        MaterialUtils.ApplySNShaders(prefab, 5f, 5f, 15);
+        var rb = prefab.AddComponent<Rigidbody>();
+        rb.useGravity = false;
+        var wf = prefab.AddComponent<WorldForces>();
+        wf.useRigidbody = rb;
+        wf.underwaterGravity = -1;
+        prefab.AddComponent<Pickupable>();
+        var placeTool = prefab.AddComponent<PlaceTool>();
+        placeTool.allowedOutside = true;
+        placeTool.rotationEnabled = true;
+        placeTool.hasAnimations = false;
+        var fpModel = prefab.AddComponent<FPModel>();
+        fpModel.propModel = prefab.transform.Find("worldmodel").gameObject;
+        fpModel.viewModel = prefab.transform.Find("fpmodel").gameObject;
+        var bounds = prefab.AddComponent<ConstructableBounds>();
+        bounds.bounds = new OrientedBounds(new Vector3(0, 0.2f, 0), Quaternion.identity, new Vector3(0.2f, 0.1f, 0.2f));
+        PrefabUtils.AddVFXFabricating(prefab, "worldmodel", -0.01f, 0.3f, default, 0.2f);
+        return prefab;
+    }
+}
---
+++ LilGargy/LilGargy/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LilGargy.Items.Equipment;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace LilGargy;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    public static AssetBundle Bundle { get; } = AssetBundleLoadingUtils.LoadFromAssetsFolder(Assembly, "lilgargy");
+
+    private void Awake()
+    {
+        // set project-scoped logger instance
+        Logger = base.Logger;
+
+        // Initialize custom prefabs
+        InitializePrefabs();
+
+        // register harmony patches, if there are any
+        Harmony.CreateAndPatchAll(Assembly, $"{PluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void InitializePrefabs()
+    {
+        LilGargyPrefab.Register();
+    }
+}
