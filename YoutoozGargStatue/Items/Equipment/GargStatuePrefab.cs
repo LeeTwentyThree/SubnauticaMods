75110c2bc8a2e23310b4aefd0f7a3efeebca75ea
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-03-23 16:11:20 -0400

    Add Youtooz Garg project

---
+++ YoutoozGargStatue/Items/Equipment/GargStatuePrefab.cs
@@ -0,0 +1,51 @@
+﻿using Nautilus.Assets;
+using Nautilus.Assets.Gadgets;
+using Nautilus.Crafting;
+using Nautilus.Utility;
+using UnityEngine;
+using Ingredient = CraftData.Ingredient;
+
+namespace YoutoozGargStatue.Items.Equipment;
+
+public static class GargStatuePrefab
+{
+    public static PrefabInfo Info { get; } = PrefabInfo
+        .WithTechType("YoutoozGargStatue", true)
+        .WithIcon(Plugin.Bundle.LoadAsset<Sprite>("YoutoozGargIcon"));
+
+    public static void Register()
+    {
+        var customPrefab = new CustomPrefab(Info);
+        
+        customPrefab.SetGameObject(GetGameObject);
+        customPrefab.SetRecipe(new RecipeData(new Ingredient(TechType.Silicone), new Ingredient(TechType.CreepvineSeedCluster)))
+            .WithFabricatorType(CraftTree.Type.Fabricator)
+            .WithStepsToFabricatorTab("Machines")
+            .WithCraftingTime(13);
+        customPrefab.SetEquipment(EquipmentType.Hand);
+        customPrefab.SetPdaGroupCategory(TechGroup.Machines, TechCategory.Machines);
+        customPrefab.Register();
+    }
+
+    private static GameObject GetGameObject()
+    {
+        var prefab = Object.Instantiate(Plugin.Bundle.LoadAsset<GameObject>("YoutoozGargPrefab"));
+        prefab.SetActive(false);
+        PrefabUtils.AddBasicComponents(prefab, Info.ClassID, Info.TechType, LargeWorldEntity.CellLevel.Near);
+        MaterialUtils.ApplySNShaders(prefab, 5.9f, 2f);
+        PrefabUtils.AddWorldForces(prefab, 14f, 0.9f, 1, true);
+        prefab.AddComponent<Pickupable>();
+        var placeTool = prefab.AddComponent<PlaceTool>();
+        placeTool.allowedOutside = true;
+        placeTool.rotationEnabled = true;
+        placeTool.hasAnimations = false;
+        placeTool.mainCollider = prefab.GetComponent<Collider>();
+        var fpModel = prefab.AddComponent<FPModel>();
+        fpModel.propModel = prefab.transform.Find("worldmodel").gameObject;
+        fpModel.viewModel = prefab.transform.Find("fpmodel").gameObject;
+        var bounds = prefab.AddComponent<ConstructableBounds>();
+        bounds.bounds = new OrientedBounds(new Vector3(0, 0.2f, 0), Quaternion.identity, new Vector3(0.2f, 0.1f, 0.2f));
+        PrefabUtils.AddVFXFabricating(prefab, "worldmodel", -0.03f, 0.5f, default, 0.5f);
+        return prefab;
+    }
+}
---
+++ YoutoozGargStatue/Plugin.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using Nautilus.Handlers;
+using Nautilus.Utility;
+using UnityEngine;
+using YoutoozGargStatue.Items.Equipment;
+
+namespace YoutoozGargStatue;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus", "1.0.0.36")]
+public class Plugin : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+    
+    public static AssetBundle Bundle { get; } = AssetBundleLoadingUtils.LoadFromAssetsFolder(Assembly, "youtoozgarg");
+
+    private void Awake()
+    {
+        LanguageHandler.RegisterLocalizationFolder();
+        
+        // set project-scoped logger instance
+        Logger = base.Logger;
+
+        // Initialize custom prefabs
+        InitializePrefabs();
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+    }
+
+    private void InitializePrefabs()
+    {
+        GargStatuePrefab.Register();
+    }
+}
