408a3d6959f12222f36c1253335c8382a1e1fcd2
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-06-25 00:07:25 -0400

    Improve positions of pocket dimension

--- PdaUpgradeChips/MonoBehaviours/Upgrades/PocketDimensionUpgradeTier1.cs
+++ PdaUpgradeChips/MonoBehaviours/Upgrades/PocketDimensionUpgradeTier1.cs
@@ -6,5 +6,5 @@ public class PocketDimensionUpgradeTier1 : PocketDimensionUpgrade
 {
     protected override TechType DimensionTechType => Plugin.PocketDimensionTier1TechType;
     protected override int DimensionTier => 1;
-    protected override Vector3 WorldLocation => new(-2500, -500, 60);
+    protected override Vector3 WorldLocation => new(-2500, 2000, 60);
 }
