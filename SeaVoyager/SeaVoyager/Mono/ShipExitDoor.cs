82ea5c7f963c34c080da7c0e00a033823ca3f24d
Law Abiding Coder <colton.trash5.5@proton.me>
2025-07-03 18:11:14 -0500

    fixed the broken door again

--- SeaVoyager/SeaVoyager/Mono/ShipExitDoor.cs
+++ SeaVoyager/SeaVoyager/Mono/ShipExitDoor.cs
@@ -19,7 +19,6 @@ namespace SeaVoyager.Mono
         public void OnHandClick(GUIHand hand)
         {
             EnterExitHelper.Exit(Player.main.transform, Player.main, false, true);
-            Player.main.SetCurrentSub(null);
             if (entrancePosition == null) return;
             Player.main.SetPosition(entrancePosition.position);
             if (sub == null) return;
