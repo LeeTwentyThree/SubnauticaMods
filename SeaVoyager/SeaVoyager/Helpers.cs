0e3506d719cf0f8fb75fc69ee2ded47a7acc4356
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-07-05 17:21:35 -0400

    Some general clean up (more to come later)

--- SeaVoyager/SeaVoyager/Helpers.cs
+++ SeaVoyager/SeaVoyager/Helpers.cs
@@ -12,12 +12,14 @@ public static class Helpers
             {
                 return child.gameObject;
             }
-            GameObject recursive = FindChild(child.gameObject, byName);
+
+            var recursive = FindChild(child.gameObject, byName);
             if (recursive)
             {
                 return recursive;
             }
         }
+
         return null;
     }
 
@@ -26,69 +28,6 @@ public static class Helpers
         var asset = ScriptableObject.CreateInstance<FMODAsset>();
         asset.path = path;
         return asset;
-    }
-
-    //I know this is horribly messy, I don't know what half the properties here do, but it works.
-    public static void ApplyMaterials(GameObject ontoObject)
-    {
-        var renderers = ontoObject.GetComponentsInChildren<Renderer>();
-        var shader = Shader.Find("MarmosetUBER");
-
-        foreach (var renderer in renderers)
-        {
-            for (int i = 0; i < renderer.materials.Length; i++)
-            {
-                Material mat = renderer.materials[i];
-                mat.shader = shader;
-                mat.SetFloat("_Glossiness", 0.6f);
-                Texture specularTexture = mat.GetTexture("_SpecGlossMap");
-                if (specularTexture != null)
-                {
-                    mat.SetTexture("_SpecTex", specularTexture);
-                    mat.SetFloat("_SpecInt", 1f);
-                    mat.SetFloat("_Shininess", 3f);
-                    mat.EnableKeyword("MARMO_SPECMAP");
-                    mat.SetColor("_SpecColor", new Color(0.796875f, 0.796875f, 0.796875f, 0.796875f));
-                    mat.SetFloat("_Fresnel", 0f);
-                    mat.SetVector("_SpecTex_ST", new Vector4(1.0f, 1.0f, 0.0f, 0.0f));
-                }
-
-                if (mat.GetTexture("_BumpMap"))
-                {
-                    mat.EnableKeyword("_NORMALMAP");
-                }
-                if (mat.name.ToLower().StartsWith("decal"))
-                {
-                    mat.EnableKeyword("MARMO_ALPHA_CLIP");
-                }
-                if (mat.name.ToLower().StartsWith("fade"))
-                {
-                    mat.EnableKeyword("_ZWRITE_ON");
-                    mat.EnableKeyword("WBOIT");
-                    mat.SetInt("_ZWrite", 0);
-                    mat.SetInt("_Cutoff", 0);
-                    mat.SetFloat("_SrcBlend", 1f);
-                    mat.SetFloat("_DstBlend", 1f);
-                    mat.SetFloat("_SrcBlend2", 0f);
-                    mat.SetFloat("_DstBlend2", 10f);
-                    mat.SetFloat("_AddSrcBlend", 1f);
-                    mat.SetFloat("_AddDstBlend", 1f);
-                    mat.SetFloat("_AddSrcBlend2", 0f);
-                    mat.SetFloat("_AddDstBlend2", 10f);
-                    mat.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack | MaterialGlobalIlluminationFlags.RealtimeEmissive;
-                    mat.renderQueue = 3101;
-                    mat.enableInstancing = true;
-
-                }
-                Texture emissionTexture = mat.GetTexture("_EmissionMap");
-                if (emissionTexture || mat.name.Contains("illum"))
-                {
-                    mat.EnableKeyword("MARMO_EMISSION");
-                    mat.SetFloat("_EnableGlow", 1f);
-                    mat.SetTexture("_Illum", emissionTexture);
-                }
-            }
-        }
     }
 }
 
@@ -102,14 +41,17 @@ public static class GameObjectExtensions
             {
                 return child.gameObject;
             }
+
             GameObject recursive = child.gameObject.SearchChild(byName);
             if (recursive)
             {
                 return recursive;
             }
         }
+
         return null;
     }
+
     public static T SearchComponent<T>(this GameObject gameObject, string gameObjectName)
     {
         foreach (Transform child in gameObject.transform)
@@ -118,12 +60,14 @@ public static class GameObjectExtensions
             {
                 return child.gameObject.GetComponent<T>();
             }
-            GameObject recursive = child.gameObject.SearchChild(gameObjectName);
+
+            var recursive = child.gameObject.SearchChild(gameObjectName);
             if (recursive)
             {
                 return recursive.GetComponent<T>();
             }
         }
+
         return default;
     }
 }
