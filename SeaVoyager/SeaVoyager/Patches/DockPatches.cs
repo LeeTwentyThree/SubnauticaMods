50f5e99993c3f3f8aff67cf1d7235a6c505eab8a
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-07-05 17:20:29 -0400

    Add back old patches for docking

---
+++ SeaVoyager/SeaVoyager/Patches/DockPatches.cs
@@ -0,0 +1,62 @@
+﻿using HarmonyLib;
+using SeaVoyager.Mono;
+using UnityEngine;
+
+namespace SeaVoyager.Patches;
+
+[HarmonyPatch]
+public static class DockPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(SeaMoth), nameof(SeaMoth.SetPlayerInside))]
+    public static void PrintSeamothControls(SeaMoth __instance, bool inside)
+    {
+        if (!inside) return;
+        PrintVehicleControls(__instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Exosuit), nameof(SeaMoth.OnPlayerEntered))]
+    public static void PrintExosuitControls(Exosuit __instance)
+    {
+        PrintVehicleControls(__instance);
+    }
+
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Player), nameof(Player.SpawnNearby))]
+    public static void SpawnNearbyOnSeaVoyager(GameObject ignoreObject, ref bool __result)
+    {
+        var vehicle = ignoreObject.GetComponent<Vehicle>();
+        if (vehicle == null) return;
+        if (Player.main.GetVehicle() != vehicle)
+        {
+            return;
+        }
+
+        var hbc = vehicle.GetComponent<HeldByCable>();
+        if (!hbc) return;
+        if (!hbc.Docked) return;
+        if (Vector3.Distance(hbc.dock.transform.position, hbc.transform.position) > 15f)
+        {
+            return;
+        }
+
+        __result = true;
+        Player.main.SetPosition(hbc.dock.ship.gameObject.FindChild("ExosuitExit").transform.position);
+        Player.main.transform.parent = null;
+    }
+
+    private static void PrintVehicleControls(Vehicle vehicle)
+    {
+        var heldByCable = vehicle.gameObject.GetComponent<HeldByCable>();
+        if (heldByCable == null) return;
+        if (heldByCable.dock == null) return;
+        ErrorMessage.AddMessage(string.Format("Return to surface: '{0}'",
+            new[] { GameInput.GetBindingName(GameInput.Button.MoveUp, GameInput.BindingSet.Primary) }));
+        ErrorMessage.AddMessage(string.Format("Descend: '{0}'",
+            new[] { GameInput.GetBindingName(GameInput.Button.MoveDown, GameInput.BindingSet.Primary) }));
+        ErrorMessage.AddMessage(string.Format("Detach cable: '{0}'",
+            new[] { GameInput.GetBindingName(GameInput.Button.Deconstruct, GameInput.BindingSet.Primary) }));
+    }
+}
