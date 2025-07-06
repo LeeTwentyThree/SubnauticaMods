1487928567daa6b6e2d2b2ef8cb9f104fe6994df
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-07-05 22:16:29 -0400

    Attempt to fix jump issue

--- SeaVoyager/SeaVoyager/SeaVoyagerConfig.cs
+++ SeaVoyager/SeaVoyager/SeaVoyagerConfig.cs
@@ -8,4 +8,6 @@ public class SeaVoyagerConfig : ConfigFile
 {
     [Toggle("Enable lower window")]
     public bool EnableCabinWindow = false;
+    [Toggle("Override player movement to fix physics issues", Label = "This setting overrides the player's current movement mode, possibly breaking other parts of the game, but improves physics when on the Sea Voyager deck. Restart required.")]
+    public bool OverrideGameMovement = true;
 }
