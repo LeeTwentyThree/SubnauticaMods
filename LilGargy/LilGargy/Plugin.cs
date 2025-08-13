224e8c26475f310eecaaf591a553e511e9d232ef
Lee23 <31892011+LeeTwentyThree@users.noreply.github.com>
2025-08-13 19:08:10 -0400

    Increase Nautilus requirement of Lil Gargy

--- LilGargy/LilGargy/Plugin.cs
+++ LilGargy/LilGargy/Plugin.cs
@@ -10,7 +10,7 @@ using UnityEngine;
 namespace LilGargy;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
-[BepInDependency("com.snmodding.nautilus")]
+[BepInDependency("com.snmodding.nautilus", "1.0.0.42")]
 public class Plugin : BaseUnityPlugin
 {
     public new static ManualLogSource Logger { get; private set; }
