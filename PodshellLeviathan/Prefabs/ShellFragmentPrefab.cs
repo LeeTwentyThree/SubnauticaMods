aef1d317e7573c37763203c216c81bc834e91d74
Kallie23 <31892011+LeeTwentyThree@users.noreply.github.com>
2026-02-06 01:40:38 -0500

    Add Podshell egg fragment

---
+++ PodshellLeviathan/Prefabs/ShellFragmentPrefab.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using Nautilus.Assets;
+using Nautilus.Utility;
+using UnityEngine;
+
+namespace PodshellLeviathan.Prefabs;
+
+public static class ShellFragmentPrefab
+{
+    public static PrefabInfo Info { get; } = PrefabInfo.WithTechType("PodshellFragment")
+        .WithIcon(Plugin.Assets.LoadAsset<Sprite>("ShellFragmentIcon"))
+        .WithSizeInInventory(new Vector2int(3, 3));
+    
+    public static void Register()
+    {
+        var prefab = new CustomPrefab(Info);
+        prefab.SetGameObject(GetPrefab);
+        prefab.Register();
+    }
+
+    private static IEnumerator GetPrefab(IOut<GameObject> result)
+    {
+        var task = Plugin.Assets.LoadAssetAsync<GameObject>("ShellFragmentPrefab");
+        yield return task;
+        var obj = Object.Instantiate(task.asset as GameObject);
+        obj.SetActive(false);
+        PrefabUtils.AddBasicComponents(obj, Info.ClassID, Info.TechType, LargeWorldEntity.CellLevel.Near);
+        MaterialUtils.ApplySNShaders(obj, 8);
+        PrefabUtils.AddWorldForces(obj, 10, 0.8f, 1.4f);
+        obj.AddComponent<Pickupable>();
+        yield return null;
+        result.Set(obj);
+    }
+}
