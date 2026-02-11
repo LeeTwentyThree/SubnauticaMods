e9c99a154969f15c7ff2407d8a00f510deb0a900
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-02-11 02:25:49 -0500

    Refactor ModAudio and add baby SFX

--- PodshellLeviathan/ModAudio.cs
+++ PodshellLeviathan/ModAudio.cs
@@ -1,5 +1,8 @@
-﻿using FMOD;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FMOD;
 using Nautilus.Extensions;
+using Nautilus.FMod;
 using Nautilus.Handlers;
 using Nautilus.Utility;
 using UnityEngine;
@@ -16,29 +19,55 @@ public static class ModAudio
     public static FMODAsset ShortRoarClose { get; } = AudioUtils.GetFmodAsset("PodshellShortRoarClose");
     public static FMODAsset ShortRoarFar { get; } = AudioUtils.GetFmodAsset("PodshellShortRoarFar");
     public static FMODAsset TeethGrinding { get; } = AudioUtils.GetFmodAsset("PodshellTeethGrinding");
+    public static FMODAsset PodshellBabyRoar { get; } = AudioUtils.GetFmodAsset("PodshellBabyRoar");
     
     // music
     public static FMODAsset PodshellMusic { get; } = AudioUtils.GetFmodAsset("PodshellMusic");
 
     public static void RegisterAudio()
     {
-        RegisterPodshellSound(CloseRoar, "close underwater roar by simon", 400);
-        RegisterPodshellSound(Death, "Death sound underwater", 400);
-        RegisterPodshellSound(Idle, "Idle_ close_ underwater", 200);
-        RegisterPodshellSound(LongRoarClose, "Long roar_ close_ underwater", 400);
-        RegisterPodshellSound(LongRoarFar, "Long roar_ far_ underwater", 400);
-        RegisterPodshellSound(ShortRoarClose, "Short roar_ close_ underwater", 400);
-        RegisterPodshellSound(ShortRoarFar, "Short roar_ far_ underwater", 400);
-        RegisterPodshellSound(TeethGrinding, "Teeth grinding underwater", 320f);
+        RegisterPodshellSound(CloseRoar, "close underwater roar by simon", 10, 400);
+        RegisterPodshellSound(Death, "Death sound underwater", 10, 400);
+        RegisterPodshellSound(Idle, "Idle_ close_ underwater", 10, 200);
+        RegisterPodshellSound(LongRoarClose, "Long roar_ close_ underwater", 10, 400);
+        RegisterPodshellSound(LongRoarFar, "Long roar_ far_ underwater", 10, 400);
+        RegisterPodshellSound(ShortRoarClose, "Short roar_ close_ underwater", 10, 400);
+        RegisterPodshellSound(ShortRoarFar, "Short roar_ far_ underwater", 10, 400);
+        RegisterPodshellSound(TeethGrinding, "Teeth grinding underwater", 10, 320f);
+        RegisterPodshellSound(PodshellBabyRoar, 1.4f, 27f, new[]
+        {
+            "PodshellBabyRoar1",
+            "PodshellBabyRoar2",
+            "PodshellBabyRoar3",
+            "PodshellBabyRoar4"
+        });
 
         RegisterMusic(PodshellMusic, "PodshellLeviathanOST", 2);
     }
 
-    private static void RegisterPodshellSound(FMODAsset asset, string clipName, float maxDistance)
+    private static void RegisterPodshellSound(FMODAsset asset, string clipName, float minDistance, float maxDistance)
     {
         var sound = AudioUtils.CreateSound(Plugin.Assets.LoadAsset<AudioClip>(clipName), AudioUtils.StandardSoundModes_3D);
-        sound.set3DMinMaxDistance(10, maxDistance);
+        sound.set3DMinMaxDistance(minDistance, maxDistance);
         CustomSoundHandler.RegisterCustomSound(asset.path, sound, AudioUtils.BusPaths.UnderwaterCreatures);
+    }
+    
+    private static void RegisterPodshellSound(FMODAsset asset, float minDistance, float maxDistance, string[] clipNames)
+    {
+        var clipList = new List<AudioClip>();
+        clipNames.ForEach(clipName => clipList.Add(Plugin.Assets.LoadAsset<AudioClip>(clipName)));
+
+        var sounds = AudioUtils.CreateSounds(clipList,
+            maxDistance >= 0 ? AudioUtils.StandardSoundModes_3D : AudioUtils.StandardSoundModes_2D);
+        sounds.ForEach(sound =>
+        {
+            if (maxDistance >= 0)
+                sound.set3DMinMaxDistance(minDistance, maxDistance);
+        });
+        
+        var multiSoundsEvent = new FModMultiSounds(sounds.ToArray(), AudioUtils.BusPaths.UnderwaterCreatures, true);
+        
+        CustomSoundHandler.RegisterCustomSound(asset.path, multiSoundsEvent);
     }
     
     private static void RegisterMusic(FMODAsset asset, string clipName, float fadeOutDuration = 10f,
