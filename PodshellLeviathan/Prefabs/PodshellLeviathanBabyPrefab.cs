6aa58477fff597e6d9755b35ee331b5fce9af397
Kallie23 <31892011+32Kallies@users.noreply.github.com>
2026-02-11 02:32:11 -0500

    Fix: baby turn speed was way too low

--- PodshellLeviathan/Prefabs/PodshellLeviathanBabyPrefab.cs
+++ PodshellLeviathan/Prefabs/PodshellLeviathanBabyPrefab.cs
@@ -31,6 +31,7 @@ public class PodshellLeviathanBabyPrefab : PodshellLeviathanPrefab
             new CustomGameObjectReference(PrefabInfo.ClassID)));
         template.AvoidObstaclesData =
             new AvoidObstaclesData(AvoidTerrainPriority, StandardSwimVelocity, false, 7, 8);
+        template.LocomotionData.forwardRotationSpeed = 0.2f;
         return template;
     }
 
